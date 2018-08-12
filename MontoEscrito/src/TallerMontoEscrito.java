2cf2f7d57e3a014795eb03736fc9ae0c783874b8
Alejo <alejandro.soto@utp.edu.co>
2018-08-11 23:21:05 -0500

    cuarto cambio.

--- MontoEscrito/src/TallerMontoEscrito.java
+++ MontoEscrito/src/TallerMontoEscrito.java
@@ -2,6 +2,22 @@
 public class TallerMontoEscrito {
 
 	public static void main(String args[]) {
-		System.out.print("Hola mundo");
+		System.out.println("Hola mundo");
+		
+		byte edad;
+		edad=32; 
+		
+		
+		System.out.println(edad);
+		
+		edad=6;
+		
+		System.out.println(edad);
+		
+		edad=26;
+		
+		System.out.println(edad);
+		
+		// sigo con el video 10.
 	}
 }
