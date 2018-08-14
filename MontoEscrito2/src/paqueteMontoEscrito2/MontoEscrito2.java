01437527c9e59d213e051be1a93cd866e01fa032
Alejo <alejandro.soto@utp.edu.co>
2018-08-14 14:09:22 -0500

    decimo cambio

--- MontoEscrito2/src/paqueteMontoEscrito2/MontoEscrito2.java
+++ MontoEscrito2/src/paqueteMontoEscrito2/MontoEscrito2.java
@@ -31,5 +31,111 @@ public class MontoEscrito2 {
 		return respuesta;
 	
 	}
+//------------------------------------------------------------
 
+	public String calcularDecena(int numero) {
+		
+		switch(numero) {
+			case 1: respuesta="diez";	
+			break;
+			case 2: respuesta="veinte";	
+			break;
+			case 3: respuesta="treinta";	
+			break;
+			case 4: respuesta="cuarenta";		
+			break;
+			case 5: respuesta="cincuenta";	
+			break;
+			case 6: respuesta="sesenta";		
+			break;
+			case 7: respuesta="setenta";	
+			break;
+			case 8: respuesta="ochenta";		
+			break;
+			case 9: respuesta="noventa";		
+			break;
+				
+		}
+		
+		return respuesta;
+	
+	}
+//-----------------------------------------------------------
+	
+	public String calcularOnce(int numero) {
+			
+			switch(numero) {
+				case 11: respuesta="once";	
+				break;
+				case 12: respuesta="doce";	
+				break;
+				case 13: respuesta="trece";	
+				break;
+				case 14: respuesta="catorce";		
+				break;
+				case 15: respuesta="quince";	
+				break;
+				case 16: respuesta="dieciseis";		
+				break;
+				case 17: respuesta="diecisiete";	
+				break;
+				case 18: respuesta="dieciocho";		
+				break;
+				case 19: respuesta="diecinueve";		
+				break;
+					
+			}
+			
+			return respuesta;
+		
+		}
+//------------------------------------------------------------	
+	
+	public String calcularOnce(int numero) {
+		
+		switch(numero) {
+			case 11: respuesta="once";	
+			break;
+			case 12: respuesta="doce";	
+			break;
+			case 13: respuesta="trece";	
+			break;
+			case 14: respuesta="catorce";		
+			break;
+			case 15: respuesta="quince";	
+			break;
+			case 16: respuesta="dieciseis";		
+			break;
+			case 17: respuesta="diecisiete";	
+			break;
+			case 18: respuesta="dieciocho";		
+			break;
+			case 19: respuesta="diecinueve";		
+			break;
+				
+		}
+		
+		return respuesta;
+	
+	}
+//-----------------------------------------------------------
+
+	public String calcularCentena(int numero) {
+		
+		switch(numero) {
+			case 1: respuesta="ciento";	
+			break;
+			case 5: respuesta="quinientos";	
+			break;
+			case 7: respuesta="setecientos";	
+			break;
+			case 9: respuesta="novecientos";		
+			break;
+			
+		}	
+		
+		return respuesta;
+	}
+	
 }
+		
