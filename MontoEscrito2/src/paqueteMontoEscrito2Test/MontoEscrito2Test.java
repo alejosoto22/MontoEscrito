e6053c0907ad578cfcf2a77631518aa3f877c70a
alejandroSoto <alejandro.soto@utp.edu.co>
2018-08-20 18:19:40 -0500

    terminando el monto escrito

--- MontoEscrito2/src/paqueteMontoEscrito2Test/MontoEscrito2Test.java
+++ MontoEscrito2/src/paqueteMontoEscrito2Test/MontoEscrito2Test.java
@@ -44,8 +44,8 @@ class MontoEscrito2Test extends MontoEscrito2 {
 	@Test
 	void testConvertir() {
 		MontoEscrito2 Convertir = new MontoEscrito2();
-		String resultadoEsperado = "mil uno";
-		String resultadoObtenido = Convertir.convertir(1001);
+		String resultadoEsperado = "mil ochenta y siete";
+		String resultadoObtenido = Convertir.convertir(1087);
 		assertEquals(resultadoEsperado, resultadoObtenido);
 	}
 
